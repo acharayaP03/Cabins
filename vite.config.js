30968548ace6b012dea8269721162d82de39087f
acharayaP03 <acharyaP03@gmail.com>
2024-10-16 20:55:17 +1100

    configure test

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [react()],
+	test: {
+		environment: 'jsdom',
+		setupFiles: 'tests/setupTests.js',
+		globals: true,
+		coverage: {
+			enabled: true, // Enable coverage reporting
+			reporter: ['text', 'html'], // Generate text and HTML reports
+			all: true, // Include all files, even those not directly tested
+			include: ['tests/**/*.{js,jsx}'], // Specify which files to include
+			exclude: ['tests/**/*.test.{js,jsx}', 'tests/setupTests.js'],
+		},
+	},
+	resolve: {
+		alias: {
+			'@': '/src',
+		},
+	},
+	server: {
+		port: 3000,
+	},
+});
