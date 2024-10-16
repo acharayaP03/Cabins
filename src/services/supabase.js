8fa4d75de66d6973e642dfa4fdc9be9446425bff
acharayaP03 <acharyaP03@gmail.com>
2024-10-17 08:13:55 +1100

    remove log

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,6 +1,5 @@
 import { createClient } from '@supabase/supabase-js';
 import { supabaseKey, supabaseUrl } from '../appConfig';
-console.log(supabaseUrl, supabaseKey);
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
