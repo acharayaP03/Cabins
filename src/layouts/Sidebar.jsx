56f1bf6aadcb175962e16d1957de75a6e7179040
acharayaP03 <acharyaP03@gmail.com>
2024-10-16 23:42:54 +1100

    fix navigatin

--- src/layouts/Sidebar.jsx
+++ src/layouts/Sidebar.jsx
@@ -1,4 +1,6 @@
 import styled from 'styled-components';
+import Logo from '@/ui/Logo';
+import MainNav from '@/ui/MainNav';
 
 const StyledSidebar = styled.aside`
 	background-color: var(--color-grey-0);
@@ -7,12 +9,17 @@ const StyledSidebar = styled.aside`
 	border-right: 1px solid var(--color-grey-100);
 
 	grid-row: 1 / -1;
+
+	display: flex;
+	flex-direction: column;
+	gap: 3.2rem;
 `;
 
 export default function Sidebar() {
 	return (
 		<StyledSidebar>
-			<p>Sidebar</p>
+			<Logo />
+			<MainNav />
 		</StyledSidebar>
 	);
 }
