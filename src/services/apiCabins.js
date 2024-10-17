b318dc4eef3a53d0ae6b0844a96625b50670d0a7
acharayaP03 <acharyaP03@gmail.com>
2024-10-18 08:11:37 +1100

    delete cabin query

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -10,3 +10,14 @@ export async function getCabins() {
 
 	return cabins;
 }
+
+export async function deleteCabin(id) {
+	let { error, data } = await supabase.from('cabins').delete().eq('id', id);
+
+	if (error) {
+		console.error(error);
+		throw new Error('Cabin could not be deleted');
+	}
+
+	return data;
+}
