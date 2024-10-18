7ad78d361f65782f00aa47271f116c5f4d59bebb
acharayaP03 <acharyaP03@gmail.com>
2024-10-18 16:41:38 +1100

    form validation

--- src/features/cabins/CreateCabinForm.jsx
+++ src/features/cabins/CreateCabinForm.jsx
@@ -47,7 +47,10 @@ const Error = styled.span`
 `;
 
 function CreateCabinForm() {
-	const { register, handleSubmit, reset } = useForm();
+	const { register, handleSubmit, reset, getValues, formState } = useForm();
+	const { errors } = formState;
+
+	console.log(errors);
 	const queryClient = useQueryClient();
 
 	const { mutate, isLoading: isCreating } = useMutation({
@@ -65,7 +68,16 @@ function CreateCabinForm() {
 		},
 	});
 
-	const handleFormSubmit = (data) => mutate(data);
+	const handleFormSubmit = (data) => {
+		mutate(data);
+	};
+
+	/**
+	 * @description this function is called when there are errors in the form, that has required fields
+	 * @param {*} errors
+	 * @returns error messages on fields that have errors
+	 */
+	const onErrors = (errors) => console.log(errors);
 
 	/**
 	 * NOTE: The `handleSubmit` function from react-hook-form is a wrapper around the native form submit event.
@@ -75,35 +87,77 @@ function CreateCabinForm() {
 	 * hence why on register function we passing sanke case keys for max_capacity, regular_price.
 	 */
 	return (
-		<Form onSubmit={handleSubmit(handleFormSubmit)}>
+		<Form onSubmit={handleSubmit(handleFormSubmit, onErrors)}>
 			<FormRow>
 				<Label htmlFor='name'>Cabin name</Label>
-				<Input type='text' id='name' {...register('name')} />
+				<Input
+					type='text'
+					id='name'
+					{...register('name', {
+						required: 'Cabin name is required',
+						minLength: { value: 3, message: 'Cabin name must be at least 3 characters long' },
+					})}
+				/>
+				{errors?.name?.message && <Error>{errors.name.message}</Error>}
 			</FormRow>
 
 			<FormRow>
 				<Label htmlFor='maxCapacity'>Maximum capacity</Label>
-				<Input type='number' id='maxCapacity' {...register('max_capacity')} />
+				<Input
+					type='number'
+					id='maxCapacity'
+					{...register('max_capacity', {
+						required: 'Maximum capacity is required',
+					})}
+				/>
+				{errors?.max_capacity?.message && <Error>{errors.max_capacity.message}</Error>}
 			</FormRow>
 
 			<FormRow>
 				<Label htmlFor='regularPrice'>Regular price</Label>
-				<Input type='number' id='regularPrice' {...register('regular_price')} />
+				<Input
+					type='number'
+					id='regularPrice'
+					{...register('regular_price', {
+						required: 'Regular price is required',
+						min: { value: 1, message: 'Regular price must be at least 1' },
+					})}
+				/>
+				{errors?.regular_price?.message && <Error>{errors.regular_price.message}</Error>}
 			</FormRow>
 
 			<FormRow>
 				<Label htmlFor='discount'>Discount</Label>
-				<Input type='number' id='discount' defaultValue={0} {...register('discount')} />
+				<Input
+					type='number'
+					id='discount'
+					defaultValue={0}
+					{...register('discount', {
+						required: 'Discount is required',
+						validate: (value) =>
+							value <= getValues().regular_price ||
+							'Discount must be less than or equal to regular price',
+					})}
+				/>
+				{errors?.discount?.message && <Error>{errors.discount.message}</Error>}
 			</FormRow>
 
 			<FormRow>
 				<Label htmlFor='description'>Description for website</Label>
-				<Textarea type='number' id='description' defaultValue='' {...register('description')} />
+				<Textarea
+					type='number'
+					id='description'
+					defaultValue=''
+					{...register('description', {
+						required: 'Description is required',
+					})}
+				/>
+				{errors?.description?.message && <Error>{errors.description.message}</Error>}
 			</FormRow>
 
 			<FormRow>
 				<Label htmlFor='image'>Cabin photo</Label>
-				<FileInput id='image' accept='image/*' {...register('image')} />
+				<FileInput id='image' accept='image/*' />
 			</FormRow>
 
 			<FormRow>
