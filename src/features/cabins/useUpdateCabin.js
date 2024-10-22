5032fb9de5e402c9a7be5d6e833f99980f2a9199
acharayaP03 <acharyaP03@gmail.com>
2024-10-22 17:10:15 +1100

    unwanted import

--- src/features/cabins/useUpdateCabin.js
+++ src/features/cabins/useUpdateCabin.js
@@ -1,5 +1,5 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
-import { updateCabin, updateCabin as updateCabinApi } from '../../services/apiCabins';
+import { updateCabin as updateCabinApi } from '../../services/apiCabins';
 import toast from 'react-hot-toast';
 
 export function useUpdateCabin() {
