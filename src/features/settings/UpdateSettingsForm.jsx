2bb0bc396dbc5add0c10143f99abc109930424ac
acharayaP03 <acharyaP03@gmail.com>
2024-10-22 20:43:44 +1100

    prefill settings from api

--- src/features/settings/UpdateSettingsForm.jsx
+++ src/features/settings/UpdateSettingsForm.jsx
@@ -1,24 +1,31 @@
-import Form from '../../ui/Form';
-import FormRow from '../../ui/FormRow';
-import Input from '../../ui/Input';
+import { Form, FormRow, Input } from '@/ui/FormComponent';
+import { useSettings } from './useSettings';
+
+import { Spinner } from '@/ui/Common';
 
 function UpdateSettingsForm() {
-  return (
-    <Form>
-      <FormRow label='Minimum nights/booking'>
-        <Input type='number' id='min-nights' />
-      </FormRow>
-      <FormRow label='Maximum nights/booking'>
-        <Input type='number' id='max-nights' />
-      </FormRow>
-      <FormRow label='Maximum guests/booking'>
-        <Input type='number' id='max-guests' />
-      </FormRow>
-      <FormRow label='Breakfast price'>
-        <Input type='number' id='breakfast-price' />
-      </FormRow>
-    </Form>
-  );
+	const { settings, isLoading } = useSettings();
+
+	if (settings === undefined) return null; // at start, settings is undefined
+	const { minimumNights, maximumNights, maximumGuests, breakfastPrice } = settings;
+
+	if (isLoading) <Spinner />;
+	return (
+		<Form>
+			<FormRow label='Minimum nights/booking'>
+				<Input type='number' id='min-nights' defaultValue={minimumNights} />
+			</FormRow>
+			<FormRow label='Maximum nights/booking'>
+				<Input type='number' id='max-nights' defaultValue={maximumNights} />
+			</FormRow>
+			<FormRow label='Maximum guests/booking'>
+				<Input type='number' id='max-guests' defaultValue={maximumGuests} />
+			</FormRow>
+			<FormRow label='Breakfast price'>
+				<Input type='number' id='breakfast-price' defaultValue={breakfastPrice} />
+			</FormRow>
+		</Form>
+	);
 }
 
 export default UpdateSettingsForm;
--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -1,7 +1,14 @@
 import { Heading } from '@/ui/Common';
+import { Row } from '@/ui/Grid';
+import UpdateSettingsForm from '@/features/settings/UpdateSettingsForm.jsx';
 
 function Settings() {
-	return <Heading as='h1'>Update hotel settings</Heading>;
+	return (
+		<Row>
+			<Heading as='h1'>Update hotel settings</Heading>
+			<UpdateSettingsForm />
+		</Row>
+	);
 }
 
 export default Settings;
