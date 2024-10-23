331962232b8d794123ab4204335872bdc4c7f0ca
acharayaP03 <acharyaP03@gmail.com>
2024-10-23 11:09:57 +1100

    applying compound component pattern

--- src/features/cabins/AddCabin.jsx
+++ src/features/cabins/AddCabin.jsx
@@ -6,22 +6,34 @@ import CreateCabinForm from './CreateCabinForm';
 function AddCabin() {
 	const [isModalOpen, setIsModalOpen] = useState(false);
 	return (
-		<div>
-			<Button variation='primary' onClick={() => setIsModalOpen((show) => !show)}>
-				Add new cabin
-			</Button>
-
-			{isModalOpen && (
-				<Modal
-					onClose={() => {
-						setIsModalOpen(false);
-					}}
-				>
-					<CreateCabinForm onCloseModal={() => setIsModalOpen(false)} />
-				</Modal>
-			)}
-		</div>
+		<Modal>
+			<Modal.Open opens='cabin-form'>
+				<Button variation='primary' onClick={() => setIsModalOpen(true)}>
+					Add new cabin
+				</Button>
+			</Modal.Open>
+			<Modal.Window name='cabin-form'>
+				<CreateCabinForm />
+			</Modal.Window>
+		</Modal>
 	);
+	// return (
+	// 	<div>
+	// 		<Button variation='primary' onClick={() => setIsModalOpen((show) => !show)}>
+	// 			Add new cabin
+	// 		</Button>
+
+	// 		{isModalOpen && (
+	// 			<Modal
+	// 				onClose={() => {
+	// 					setIsModalOpen(false);
+	// 				}}
+	// 			>
+	// 				<CreateCabinForm onCloseModal={() => setIsModalOpen(false)} />
+	// 			</Modal>
+	// 		)}
+	// 	</div>
+	// );
 }
 
 export default AddCabin;
