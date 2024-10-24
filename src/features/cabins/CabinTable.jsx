66d9ade4a9fc682245899c798215b78bf649da7d
Prabhakar acharya <acharyap03@gmail.com>
2024-10-24 17:02:07 +1100

    use of menu component

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -3,24 +3,27 @@ import { useCabins } from './useCabins';
 import CabinRow from './CabinRow';
 import Spinner from '../../ui/Common/Spinner';
 import Table from '@/ui/Tables/Table';
+import Menus from '@/ui/ActionMenu/Menus';
 
 function CabinTable() {
-	const { isLoading, cabins, error } = useCabins();
+	const { isLoading, cabins } = useCabins();
 	if (isLoading) return <Spinner />;
 
 	return (
-		<Table columns='0.6fr 1.8fr 2.2fr 1fr 1fr 1fr'>
-			<Table.Header>
-				<div role='columnheader'></div>
-				<div role='columnheader'>Cabin</div>
-				<div role='columnheader'>Capacity</div>
-				<div role='columnheader'>Price</div>
-				<div role='columnheader'>Discount</div>
-				<div></div>
-			</Table.Header>
+		<Menus>
+			<Table columns='0.6fr 1.8fr 2.2fr 1fr 1fr 1fr'>
+				<Table.Header>
+					<div role='columnheader'></div>
+					<div role='columnheader'>Cabin</div>
+					<div role='columnheader'>Capacity</div>
+					<div role='columnheader'>Price</div>
+					<div role='columnheader'>Discount</div>
+					<div></div>
+				</Table.Header>
 
-			<Table.Body data={cabins} render={(cabin) => <CabinRow key={cabin.id} cabin={cabin} />} />
-		</Table>
+				<Table.Body data={cabins} render={(cabin) => <CabinRow key={cabin.id} cabin={cabin} />} />
+			</Table>
+		</Menus>
 	);
 }
 
