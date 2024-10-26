76197a119ffd7a927323272a961496f15c00dbe8
Prabhakar acharya <acharyap03@gmail.com>
2024-10-27 07:58:34 +1100

    making method for filter dynamic so that later user can pass any filters

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -6,14 +6,22 @@ export function useBookings() {
 	const [searchParams] = useSearchParams();
 	const filterStatus = searchParams.get('status') || 'all';
 
+	/**
+	 * inorder for filters to work, we need to pass below object to getAllBookings
+	 * @fields {string} - field to filter on
+	 * @value {string} - value to filter on
+	 * @method {string} - method to filter db query (eq, gte, lte, etc)(default is eq) these are supabase methods
+	 */
 	const filters =
-		!filterStatus || filterStatus === 'all' ? null : { field: 'status', value: filterStatus };
+		!filterStatus || filterStatus === 'all'
+			? null
+			: { field: 'status', value: filterStatus, method: 'eq' };
 	const {
 		isLoading,
 		data: bookings,
 		error,
 	} = useQuery({
-		queryKey: ['bookings'],
+		queryKey: ['bookings', filters], // adding filters as dependency to refetch when filters change
 		queryFn: () => getAllBookings({ filters }),
 	});
 
