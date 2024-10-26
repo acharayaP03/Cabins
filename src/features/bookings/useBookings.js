58fcacd9e24e1106a904f87e4392386594781d3d
Prabhakar acharya <acharyap03@gmail.com>
2024-10-27 07:47:27 +1100

    initial extract of search query params

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -1,14 +1,20 @@
 import { useQuery } from '@tanstack/react-query';
 import { getAllBookings } from '@/services/apiBookings';
+import { useSearchParams } from 'react-router-dom';
 
 export function useBookings() {
+	const [searchParams] = useSearchParams();
+	const filterStatus = searchParams.get('status') || 'all';
+
+	const filters =
+		!filterStatus || filterStatus === 'all' ? null : { field: 'status', value: filterStatus };
 	const {
 		isLoading,
 		data: bookings,
 		error,
 	} = useQuery({
 		queryKey: ['bookings'],
-		queryFn: getAllBookings,
+		queryFn: () => getAllBookings({ filters }),
 	});
 
 	return { isLoading, bookings, error };
