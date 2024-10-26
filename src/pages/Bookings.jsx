8db49448ef4b24cf102b812f4b4177d7b7724697
Prabhakar acharya <acharyap03@gmail.com>
2024-10-27 07:28:59 +1100

    get booking filter componet done

--- src/pages/Bookings.jsx
+++ src/pages/Bookings.jsx
@@ -1,12 +1,14 @@
 import { Heading } from '@/ui/Common';
 import { Row } from '@/ui/Grid';
 import BookingTable from '@/features/bookings/BookingTable';
+import BookingTableFilters from '../features/bookings/BookingTableFilters';
 
 function Bookings() {
 	return (
 		<>
 			<Row type='horizontal'>
 				<Heading as='h1'>All bookings</Heading>
+				<BookingTableFilters />
 			</Row>
 			<Row>
 				<BookingTable />
