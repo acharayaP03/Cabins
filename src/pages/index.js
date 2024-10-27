ea08d22b7c929fd4b40732a8beea2a4c67409ea9
Prabhakar acharya <acharyap03@gmail.com>
2024-10-27 19:05:42 +1100

    presentational component for booking description

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import Account from './Account';
 import Bookings from './Bookings';
+import Booking from './Booking';
 import Cabins from './Cabins';
 import Dashboard from './Dashboard';
 import Login from './Login';
@@ -7,4 +8,4 @@ import PageNotFound from './PageNotFound';
 import Settings from './Settings';
 import Users from './Users';
 
-export { Account, Bookings, Cabins, Dashboard, Login, PageNotFound, Settings, Users };
+export { Account, Bookings, Booking, Cabins, Dashboard, Login, PageNotFound, Settings, Users };
