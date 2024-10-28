a80908aeeff94e6f564a770d5d68958b9a6bfd6f
Prabhakar acharya <acharyap03@gmail.com>
2024-10-28 21:41:13 +1100

    checkin in guest

--- src/features/check-in-out/CheckinBooking.jsx
+++ src/features/check-in-out/CheckinBooking.jsx
@@ -6,8 +6,11 @@ import { Row } from '@/ui/Grid';
 import { Heading, Spinner } from '@/ui/Common';
 import { Checkbox } from '../../ui/FormComponent';
 import { ButtonGroup, Button, ButtonText } from '@/ui/Buttons';
+
 import { useMoveBack } from '@/hooks/useMoveBack';
 import { useBooking } from '../bookings/useBooking';
+import { formatCurrency } from '@/utils/helpers';
+import { useCheckin } from './useCheckin';
 
 const Box = styled.div`
 	/* Box */
@@ -21,6 +24,7 @@ function CheckinBooking() {
 	const [confirmPaid, setConfirmPaid] = useState(false);
 	const { booking, isLoading } = useBooking();
 	const moveBack = useMoveBack();
+	const { checkin, isCheckingIn } = useCheckin();
 
 	useEffect(() => {
 		setConfirmPaid(booking?.is_paid ?? false);
@@ -40,6 +44,11 @@ function CheckinBooking() {
 		setConfirmPaid((confirm) => !confirm);
 	}
 
+	function handleClick() {
+		if (!confirmPaid) return;
+		checkin(bookingId);
+	}
+
 	return (
 		<>
 			<Row type='horizontal'>
@@ -55,13 +64,14 @@ function CheckinBooking() {
 					checked={confirmPaid}
 					onChange={handleCheckin}
 					id='confirm'
-					disabled={confirmPaid}
+					disabled={confirmPaid || isCheckingIn}
 				>
-					I confirm that {guests.full_name} has paid the total amount.
+					I confirm that {guests.full_name} has paid the total amount of{' '}
+					{formatCurrency(totalPrice)}.
 				</Checkbox>
 			</Box>
 			<ButtonGroup>
-				<Button onClick={handleCheckin} disabled={!confirmPaid}>
+				<Button onClick={handleClick} disabled={!confirmPaid}>
 					Check in booking #{bookingId}
 				</Button>
 				<Button variation='secondary' onClick={moveBack}>
