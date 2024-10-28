9d4926e4a5ed28f5e4ed208e0165105608aaece7
Prabhakar acharya <acharyap03@gmail.com>
2024-10-29 08:09:51 +1100

    add additional breakfast with total price break down in ui

--- src/features/check-in-out/CheckinBooking.jsx
+++ src/features/check-in-out/CheckinBooking.jsx
@@ -11,6 +11,7 @@ import { useMoveBack } from '@/hooks/useMoveBack';
 import { useBooking } from '../bookings/useBooking';
 import { formatCurrency } from '@/utils/helpers';
 import { useCheckin } from './useCheckin';
+import { useSettings } from '../settings/useSettings';
 
 const Box = styled.div`
 	/* Box */
@@ -22,15 +23,17 @@ const Box = styled.div`
 
 function CheckinBooking() {
 	const [confirmPaid, setConfirmPaid] = useState(false);
+	const [addBreakfast, setAddBreakfast] = useState(false);
 	const { booking, isLoading } = useBooking();
 	const moveBack = useMoveBack();
 	const { checkin, isCheckingIn } = useCheckin();
+	const { settings, isLoading: isLoadingSettings } = useSettings();
 
 	useEffect(() => {
 		setConfirmPaid(booking?.is_paid ?? false);
 	}, [booking]);
 
-	if (isLoading) return <Spinner />;
+	if (isLoading & isLoadingSettings) return <Spinner />;
 	const {
 		id: bookingId,
 		guests,
@@ -39,6 +42,13 @@ function CheckinBooking() {
 		has_breakfast: hasBreakfast,
 		num_nights: numNights,
 	} = booking;
+
+	// calculate breakfast price
+	const optionBreakfastPrice = settings?.breakfastPrice * numGuests * numNights;
+
+	const totalPriceWithBreakfast = formatCurrency(totalPrice + optionBreakfastPrice);
+	const totalPriceBreadDown =
+		formatCurrency(totalPrice) + ' + ' + formatCurrency(optionBreakfastPrice);
 
 	function handleCheckin() {
 		setConfirmPaid((confirm) => !confirm);
@@ -57,6 +67,22 @@ function CheckinBooking() {
 			</Row>
 
 			<BookingDataBox booking={booking} />
+
+			{!hasBreakfast && (
+				<Box>
+					<Checkbox
+						checked={addBreakfast}
+						onChange={() => {
+							setAddBreakfast((add) => !add);
+							setConfirmPaid(false);
+						}}
+						id='addBreakfast'
+					>
+						Add breakfast for {numGuests} guests for {numNights} nights for a total of{' '}
+						{formatCurrency(optionBreakfastPrice)}.
+					</Checkbox>
+				</Box>
+			)}
 			{/* TODO: on confirm check box, it needs to disabled when payment is received when check in is commited. */}
 			{/* the query will be refetched and this checkbox will be disabled. at the moment we will live with this bug. */}
 			<Box>
@@ -67,7 +93,10 @@ function CheckinBooking() {
 					disabled={confirmPaid || isCheckingIn}
 				>
 					I confirm that {guests.full_name} has paid the total amount of{' '}
-					{formatCurrency(totalPrice)}.
+					{!addBreakfast
+						? formatCurrency(totalPrice)
+						: `${totalPriceWithBreakfast} including breakfast ( ${totalPriceBreadDown} ) `}
+					.
 				</Checkbox>
 			</Box>
 			<ButtonGroup>
