d86a8ae4602d6b31b31f367c3a84b9750ab57eb4
Prabhakar acharya <acharyap03@gmail.com>
2024-10-29 17:02:01 +1100

    use user hook

--- src/features/authentication/useLogin.js
+++ src/features/authentication/useLogin.js
@@ -1,17 +1,18 @@
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { login as loginApi } from '../../services/apiAuthentication';
 import toast from 'react-hot-toast';
 import { useNavigate } from 'react-router-dom';
 
 export function useLogin() {
 	const navigate = useNavigate();
+	const queryClient = useQueryClient();
+
 	const { mutate: login, isLoading: isLogingIn } = useMutation({
 		mutationFn: ({ email, password }) => {
 			return loginApi({ email, password });
 		},
 		onSuccess: (user) => {
-			console.log('User logged in:', user);
-			toast.success('Logged in successfully');
+			queryClient.setQueriesData(['user'], user); // update the user query with the new user
 			navigate('/dashboard');
 		},
 		onError: (error) => {
