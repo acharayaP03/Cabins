cb7079dfa2a18e5d825d22425f5185a7d583ec10
Prabhakar acharya <acharyap03@gmail.com>
2024-10-29 15:54:17 +1100

    using it for login

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -1,13 +1,20 @@
 import { useState } from 'react';
 import { Button } from '@/ui/Buttons';
 import { Form, Input, FormRowVertical } from '@/ui/FormComponent';
+import { SpinnerMini } from '@/ui/Common';
+import { useLogin } from './useLogin';
 
 export default function LoginForm() {
 	const [email, setEmail] = useState('');
 	const [password, setPassword] = useState('');
+	const { login, isLogingIn } = useLogin();
 
-	function handleSubmit() {}
+	function handleSubmit(e) {
+		e.preventDefault();
+		login({ email, password });
+	}
 
+	if (isLogingIn) <SpinnerMini />;
 	return (
 		<Form onSubmit={handleSubmit}>
 			<FormRowVertical label='Email address'>
@@ -17,6 +24,7 @@ export default function LoginForm() {
 					// This makes this form better for password managers
 					autoComplete='username'
 					value={email}
+					disabled={isLogingIn}
 					onChange={(e) => setEmail(e.target.value)}
 				/>
 			</FormRowVertical>
@@ -26,11 +34,14 @@ export default function LoginForm() {
 					id='password'
 					autoComplete='current-password'
 					value={password}
+					disabled={isLogingIn}
 					onChange={(e) => setPassword(e.target.value)}
 				/>
 			</FormRowVertical>
 			<FormRowVertical>
-				<Button size='large'>Login</Button>
+				<Button size='large' disabled={isLogingIn}>
+					{isLogingIn ? <SpinnerMini /> : 'Login'}
+				</Button>
 			</FormRowVertical>
 		</Form>
 	);
