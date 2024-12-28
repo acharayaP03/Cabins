0db43bf04ee963ac9b90083db070b903a67d86fa
Prabhakar acharya <acharyap03@gmail.com>
2024-12-28 20:41:17 +1100

    initial dashboard layout

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,12 +1,17 @@
+import DashboardFilter from '../features/dashboard/DashboardFilter';
+import DashboardLayout from '../features/dashboard/DashboardLayout';
 import Heading from '../ui/Common/Heading';
 import Row from '../ui/Grid/Row';
 
 function Dashboard() {
 	return (
-		<Row type='horizontal'>
-			<Heading as='h1'>Dashboard</Heading>
-			<p>TEST</p>
-		</Row>
+		<>
+			<Row type='horizontal'>
+				<Heading as='h1'>Dashboard</Heading>
+				<DashboardFilter />
+			</Row>
+			<DashboardLayout />
+		</>
 	);
 }
 
