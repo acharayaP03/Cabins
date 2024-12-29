0e8554da7e864faf9305fc154a8173989327b636
Prabhakar acharya <acharyap03@gmail.com>
2024-12-29 16:10:42 +1100

    prettier formatter

--- src/features/bookings/BookingTable.jsx
+++ src/features/bookings/BookingTable.jsx
@@ -1,37 +1,39 @@
-import BookingRow from './BookingRow';
-import { Table, Pagination } from '@/ui/Tables';
-import { Menus } from '@/ui/ActionControls';
-import { Empty, Spinner } from '@/ui/Common';
-import { useBookings } from './useBookings';
+import BookingRow from "./BookingRow";
+import { Table, Pagination } from "@/ui/Tables";
+import { Menus } from "@/ui/ActionControls";
+import { Empty, Spinner } from "@/ui/Common";
+import { useBookings } from "./useBookings";
 
 function BookingTable() {
-	const { isLoading, bookings, count } = useBookings();
+    const { isLoading, bookings, count } = useBookings();
 
-	if (isLoading) return <Spinner />;
-	if (!bookings.length) <Empty resourceName='bookings' />;
+    if (isLoading) return <Spinner />;
+    if (!bookings.length) <Empty resourceName="bookings" />;
 
-	return (
-		<Menus>
-			<Table columns='0.6fr 2fr 2.4fr 1.4fr 1fr 3.2rem'>
-				<Table.Header>
-					<div>Cabin</div>
-					<div>Guest</div>
-					<div>Dates</div>
-					<div>Status</div>
-					<div>Amount</div>
-					<div></div>
-				</Table.Header>
+    return (
+        <Menus>
+            <Table columns="0.6fr 2fr 2.4fr 1.4fr 1fr 3.2rem">
+                <Table.Header>
+                    <div>Cabin</div>
+                    <div>Guest</div>
+                    <div>Dates</div>
+                    <div>Status</div>
+                    <div>Amount</div>
+                    <div></div>
+                </Table.Header>
 
-				<Table.Body
-					data={bookings}
-					render={(booking) => <BookingRow key={booking.id} booking={booking} />}
-				/>
-				<Table.Footer>
-					<Pagination count={count} />
-				</Table.Footer>
-			</Table>
-		</Menus>
-	);
+                <Table.Body
+                    data={bookings}
+                    render={(booking) => (
+                        <BookingRow key={booking.id} booking={booking} />
+                    )}
+                />
+                <Table.Footer>
+                    <Pagination count={count} />
+                </Table.Footer>
+            </Table>
+        </Menus>
+    );
 }
 
 export default BookingTable;
