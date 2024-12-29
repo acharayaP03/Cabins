0cc2ca9cfcc8c457945903a61295636501e573ac
Prabhakar acharya <acharyap03@gmail.com>
2024-12-29 11:19:46 +1100

    formatted with prettier

--- src/layouts/Sidebar.jsx
+++ src/layouts/Sidebar.jsx
@@ -1,26 +1,28 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
-import { Logo } from '@/ui/Common';
-import { MainNav } from '@/ui/ActionControls';
+import { Logo } from "@/ui/Common";
+import { MainNav } from "@/ui/ActionControls";
+import Uploader from "../data/Uploader";
 
 const StyledSidebar = styled.aside`
-	background-color: var(--color-grey-0);
-	padding: 2.4rem;
-	padding: 3.2rem 2.4rem;
-	border-right: 1px solid var(--color-grey-100);
+    background-color: var(--color-grey-0);
+    padding: 2.4rem;
+    padding: 3.2rem 2.4rem;
+    border-right: 1px solid var(--color-grey-100);
 
-	grid-row: 1 / -1;
+    grid-row: 1 / -1;
 
-	display: flex;
-	flex-direction: column;
-	gap: 3.2rem;
+    display: flex;
+    flex-direction: column;
+    gap: 3.2rem;
 `;
 
 export default function Sidebar() {
-	return (
-		<StyledSidebar>
-			<Logo />
-			<MainNav />
-		</StyledSidebar>
-	);
+    return (
+        <StyledSidebar>
+            <Logo />
+            <MainNav />
+            <Uploader />
+        </StyledSidebar>
+    );
 }
